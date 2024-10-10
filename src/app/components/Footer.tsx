49ad470909a60acec3601f369699a69d1f513dfc
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-09 21:43:31 -0300

    first commit

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="w-full bg-black p-5 flex justify-center items-center text-white">
+            <p className="text-center">
+                Integrantes: <br />
+                <span className="font-bold">Caio Marques RM 555997 Caio Amarante RM 558640 Felipe Camarg RM 556325</span>
+                <br /><a href="">GitHub</a>
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/app/components/Header.tsx
@@ -0,0 +1,8 @@
+const Header = () => {
+    return (
+        <header className="w-full bg-black p-5 flex justify-center items-center">
+            <h1 className="text-pink-700 text-4xl font-bold">FiapNews</h1>
+        </header>
+    );
+}
+export default Header
